c88e0ad0210770353b338f21c7db33b2ee60247e
Go Away <baltzer.matt@gmail.com>
2024-07-24 22:37:12 -0700

    Set up the main file and the settings file. Started importing the player image

--- code/main.py
+++ code/main.py
@@ -1,27 +1,40 @@
 # Example file showing a basic pygame "game loop"
 import pygame
+from settings import *
+from os.path import join
 
-# pygame setup
-pygame.init()
-screen = pygame.display.set_mode((1280, 720))
-clock = pygame.time.Clock()
-running = True
+class Player(pygame.sprite.Sprite):
+    def __init__(self, surf, pos, groups):
+        super().__init__(groups)
+        self.surf = surf
+        self.pos = pos
+    
+    def update(self, dt):
+        pass
 
-while running:
-    # poll for events
-    # pygame.QUIT event means the user clicked X to close your window
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
+class Game():
+    def __init__(self):
+        pygame.init()
+        self.display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+        pygame.display.set_caption('Gun Survivor')
+        self.clock = pygame.time.Clock()
 
-    # fill the screen with a color to wipe away anything from last frame
-    screen.fill("purple")
+    def import_assets(self):
+        self.player_surf = [pygame.image.load(join('.', 'images', 'player', 'down', f'{i}.png')).convert_alpha() for i in range(4)]
 
-    # RENDER YOUR GAME HERE
+    def run(self):
+        while True:
+            dt = self.clock.tick() / 1000
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
 
-    # flip() the display to put your work on screen
-    pygame.display.flip()
+            self.display_surface.fill("black")
+            pygame.display.update()
 
-    clock.tick(60)  # limits FPS to 60
+            self.clock.tick(60)
 
-pygame.quit()
+if __name__ == '__main__':
+    game = Game()
+    game.run()
