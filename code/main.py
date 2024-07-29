8409aab4812d34a5978f13ae61abe546b7895b1f
Go Away <baltzer.matt@gmail.com>
2024-07-29 14:07:48 -0700

    Updated the main file, removed the Player class. Created separate file for the Player. Updated the player with movement and an image

--- code/main.py
+++ code/main.py
@@ -1,40 +1,48 @@
 # Example file showing a basic pygame "game loop"
 import pygame
 from settings import *
+from player import Player
 from os.path import join
-
-class Player(pygame.sprite.Sprite):
-    def __init__(self, surf, pos, groups):
-        super().__init__(groups)
-        self.surf = surf
-        self.pos = pos
-    
-    def update(self, dt):
-        pass
 
 class Game():
     def __init__(self):
+        # main setup
         pygame.init()
         self.display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
         pygame.display.set_caption('Gun Survivor')
         self.clock = pygame.time.Clock()
+        self.running = True
+
+        # groups
+        self.all_sprites = pygame.sprite.Group()
+
+        # sprites
+        self.player = Player((400,300), self.all_sprites)
 
     def import_assets(self):
         self.player_surf = [pygame.image.load(join('.', 'images', 'player', 'down', f'{i}.png')).convert_alpha() for i in range(4)]
 
     def run(self):
-        while True:
+        while self.running:
             dt = self.clock.tick() / 1000
+            
+            # event loop
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
-                    pygame.quit()
-                    sys.exit()
+                    self.running = False
 
-            self.display_surface.fill("black")
+            # update
+            self.display_surface.fill('black')
+            self.all_sprites.update(dt)
+
+            # drawing
+            self.all_sprites.draw(self.display_surface)
             pygame.display.update()
 
-            self.clock.tick(60)
+            # self.clock.tick(60)
+        pygame.quit()
 
+# Checks if the file is the current main file, if so it runs the game
 if __name__ == '__main__':
     game = Game()
     game.run()
