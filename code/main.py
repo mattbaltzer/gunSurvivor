5b5d51f8bb20994c7fe3743b210c7c52367eba3a
Go Away <baltzer.matt@gmail.com>
2024-07-29 15:02:22 -0700

    Created a sprite class to handle basic collisions on the map. Added in a loop to check if the player is colliding with an object from any direction

--- code/main.py
+++ code/main.py
@@ -2,7 +2,9 @@
 import pygame
 from settings import *
 from player import Player
-from os.path import join
+from sprites import *
+
+from random import randint
 
 class Game():
     def __init__(self):
@@ -15,9 +17,16 @@ class Game():
 
         # groups
         self.all_sprites = pygame.sprite.Group()
+        self.collision_sprites = pygame.sprite.Group()
 
         # sprites
-        self.player = Player((400,300), self.all_sprites)
+        # Putting the collision sprites at the end of Player makes it an arguement and allows the player to access the group, it isn't in the Collision Sprites group
+        self.player = Player((400,300), self.all_sprites, self.collision_sprites)
+        for i in range(6):
+            x, y = randint(0, WINDOW_WIDTH), randint(0, WINDOW_HEIGHT)
+            w, h = randint(60, 100), randint(50, 100)
+            # Putting both sprites in a tuple has them join into the same group
+            CollisionSprite((x, y), (w, h), (self.all_sprites, self.collision_sprites))
 
     def import_assets(self):
         self.player_surf = [pygame.image.load(join('.', 'images', 'player', 'down', f'{i}.png')).convert_alpha() for i in range(4)]
--- code/player.py
+++ code/player.py
@@ -1,7 +1,7 @@
 from settings import *
 
 class Player(pygame.sprite.Sprite):
-    def __init__(self, pos, groups):
+    def __init__(self, pos, groups, collision_sprites):
         super().__init__(groups)
         self.image = pygame.image.load(join('.', 'images', 'player', 'down', '0.png')).convert_alpha()
         self.rect = self.image.get_frect(center = pos)
@@ -9,6 +9,7 @@ class Player(pygame.sprite.Sprite):
         # movement
         self.direction = pygame.Vector2()
         self.speed = 500
+        self.collision_sprites = collision_sprites
 
     def input(self):
         keys = pygame.key.get_pressed()
@@ -17,8 +18,26 @@ class Player(pygame.sprite.Sprite):
         self.direction = self.direction.normalize() if self.direction else self.direction
 
     def move(self, dt):
-        self.rect.center += self.direction * self.speed * dt
+        self.rect.x += self.direction.x * self.speed * dt
+        self.collision('horizontal')
+        self.rect.y += self.direction.y * self.speed * dt
+        self.collision('vertical')
         
+    def collision(self, direction):
+        for sprite in self.collision_sprites:
+            if sprite.rect.colliderect(self.rect):
+                # This sets up basic collisions for the player and a sprite, checks the horizontal and vertical movements
+                if direction =='horizontal':
+                    if self.direction.x > 0:
+                        self.rect.right = sprite.rect.left
+                    if self.direction.x < 0:
+                        self.rect.left = sprite.rect.right
+                if direction == 'vertical':
+                    if self.direction.y > 0:
+                        self.rect.bottom = sprite.rect.top
+                    if self.direction.y < 0:
+                        self.rect.top = sprite.rect.bottom
+
     def update(self, dt):
         self.input()
         self.move(dt)
